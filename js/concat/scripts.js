cfde6d48310714a2be3f927fe98ce9ca7ba4e7d9
agustin <castiarena@gmail.com>
2015-11-09 03:07:53 -0300

    terminado el ejercicio

--- js/concat/scripts.js
+++ js/concat/scripts.js
@@ -20511,11 +20511,31 @@ ch.factory = function (Klass, fn) {
 }(this.ch.Autocomplete, this.ch)); var PRODUCTO = {
     data: null,
     carousel: null,
+    tabs: null,
+    form: document.getElementById('comprar'),
     init: function(){
         this.setCarousel();
+        this.setTabs();
+        this.actions();
     },
     setCarousel: function(){
-        this.carousel = $('#carousel').carousel();
+        this.carousel = $('#carousel').carousel({pagination:true});
+    },
+    setTabs: function(){
+        this.tabs = $('#tabs').tabs();
+    },
+    actions: function(){
+        this.form.addEventListener('submit',function(e){
+            e.preventDefault();
+            var statusBuy = $('.ch-box-ok[data-buy]');
+            statusBuy.html('Gracias por comprar un <b>'+ statusBuy.attr('data-buy').toUpperCase().replace(new RegExp('-'),' ')+'</b> en MercadoLibre');
+            statusBuy.fadeIn(function(){
+                setTimeout(function(){
+                    statusBuy.fadeOut();
+                },500);
+            });
+
+        })
     }
 };
 
--- js/src/scripts.js
+++ js/src/scripts.js
@@ -1,11 +1,31 @@
 var PRODUCTO = {
     data: null,
     carousel: null,
+    tabs: null,
+    form: document.getElementById('comprar'),
     init: function(){
         this.setCarousel();
+        this.setTabs();
+        this.actions();
     },
     setCarousel: function(){
-        this.carousel = $('#carousel').carousel();
+        this.carousel = $('#carousel').carousel({pagination:true});
+    },
+    setTabs: function(){
+        this.tabs = $('#tabs').tabs();
+    },
+    actions: function(){
+        this.form.addEventListener('submit',function(e){
+            e.preventDefault();
+            var statusBuy = $('.ch-box-ok[data-buy]');
+            statusBuy.html('Gracias por comprar un <b>'+ statusBuy.attr('data-buy').toUpperCase().replace(new RegExp('-'),' ')+'</b> en MercadoLibre');
+            statusBuy.fadeIn(function(){
+                setTimeout(function(){
+                    statusBuy.fadeOut();
+                },500);
+            });
+
+        })
     }
 };
 
